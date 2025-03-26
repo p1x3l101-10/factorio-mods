caeab06db2ca6d7cf8203d893d4f36f65aff2c3b
Pixel <scott.blatt.0b10@gmail.com>
2025-03-26 15:02:18 -0500

    rename

--- src/compatibility/lignumis/scripts/fix-tech-unlocks.lua
+++ src/compatibility/lignumis/scripts/fix-tech-unlocks.lua
@@ -1,12 +1,12 @@
 -- Re-lock lignumis because it looks bad
-local InitNew = {}
+local FixTechUnlocks = {}
 
 InitNew.on_init = function()
   if game.tick > 0 then return end
 
-  if not force.technologies["planet-discovery-nauvis"].researched then
-    force.technologies["planet-discovery-lignumis"].researched = false
+  if not game.player.force.technologies["planet-discovery-nauvis"].researched then
+    game.player.force.technologies["planet-discovery-lignumis"].researched = false
   end
 end
 
-return InitNew
+return FixTechUnlocks
