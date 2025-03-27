aa9d562049945b9fa8208d925e147992d42faf21
Pixel <scott.blatt.0b10@gmail.com>
2025-03-27 11:24:29 -0500

    change defaults

--- src/settings/peaceful-nauvis.lua
+++ src/settings/peaceful-nauvis.lua
@@ -3,6 +3,6 @@ data:extend({
     type = "bool-setting",
     name = "hexalith-nuclear-science-peaceful-nauvis",
     setting_type = "startup",
-    default_value = true
+    default_value = false
   }
 })
