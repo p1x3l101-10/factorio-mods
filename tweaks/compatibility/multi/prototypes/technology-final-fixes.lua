fea8de6e67a5cfb7eafc77c53e125a4aad864f3a
Pixel <scott.blatt.0b10@gmail.com>
2025-03-28 11:53:40 -0500

    hide empty ammo until nauvis

--- tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
+++ tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
@@ -1,3 +1,12 @@
 if mods["alloy-smelting"] and mods["lignumis"] then
   data.raw["technology"]["kiln-smelting"].prerequisites = { "planet-discovery-nauvis" }
 end
+
+if mods["lignumis"] and mods["More_Ammo"] then
+  local recipes = data.raw["recipe"]
+  recipes["empty-magazine"].enabled = false
+  recipes["empty-shotgun-shell"].enabled = false
+  local discover_nauvis = data.raw["technology"]["planet-discovery-nauvis"]
+  discover_nauvis.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-magazine" }
+  discover_nauvis.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-shotgun-shell" }
+end
