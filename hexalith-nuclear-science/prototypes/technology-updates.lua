ae843793c2f0635c1c68b0cdfdb4850de46c0c31
Pixel <scott.blatt.0b10@gmail.com>
2025-03-28 09:23:19 -0500

    hide uranium ore

--- hexalith-nuclear-science/prototypes/technology-updates.lua
+++ hexalith-nuclear-science/prototypes/technology-updates.lua
@@ -6,3 +6,8 @@ util.add_prerequisite("uranium-mining", "planet-discovery-hexalith")
 util.delete_prerequisite("planet-discovery-hexalith", "space-science-pack")
 util.delete_prerequisite("planet-discovery-hexalith", "power-armor-mk2")
 util.add_prerequisite("planet-discovery-hexalith", "space-platform-thruster")
+
+-- Hide uranium until you go to hexalith
+data.raw.recipe["uranium-ore"].enabled = false
+local tech = data.raw.technology["uranium-mining"]
+tech.effects[#tech.effects + 1 ] = { type = "unlock-recipe", recipe = "uranium-ore"}
