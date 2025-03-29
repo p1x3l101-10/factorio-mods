97d89f4b565e275757481e2a337445fff499de5d
Pixel <scott.blatt.0b10@gmail.com>
2025-03-29 16:41:12 -0500

    move some tech around

--- tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
+++ tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
@@ -1,12 +1,12 @@
-if mods["alloy-smelting"] and mods["lignumis"] then
-  data.raw["technology"]["kiln-smelting"].prerequisites = { "planet-discovery-nauvis" }
+if mods["alloy-smelting"] and mods["lignumis"] and not mods["aai-industry"] then
+  data.raw["technology"]["kiln-smelting"].prerequisites = { "automation-science-pack" }
 end
 
 if mods["lignumis"] and mods["More_Ammo"] then
   local recipes = data.raw["recipe"]
   recipes["empty-recipe"].enabled = false
   recipes["empty-shotgun-recipe"].enabled = false
-  local discover_nauvis = data.raw["technology"]["planet-discovery-nauvis"]
-  discover_nauvis.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-recipe" }
-  discover_nauvis.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-shotgun-recipe" }
+  local iron_processing = data.raw["technology"]["iron-processing"]
+  iron_processing.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-recipe" }
+  iron_processing.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-shotgun-recipe" }
 end
