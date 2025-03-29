3bc15c69dd15d3b2d05ef8658eed11ebbbf8df18
Pixel <scott.blatt.0b10@gmail.com>
2025-03-29 16:46:07 -0500

    typo

--- tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
+++ tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
@@ -7,8 +7,8 @@ if mods["lignumis"] and mods["More_Ammo"] then
   recipes["empty-recipe"].enabled = false
   recipes["empty-shotgun-recipe"].enabled = false
   local iron_processing = data.raw["technology"]["iron-processing"]
-  iron_processing.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-recipe" }
-  iron_processing.effects[#discover_nauvis.effects + 1] = { type = "unlock-recipe", recipe = "empty-shotgun-recipe" }
+  iron_processing.effects[#iron_processing.effects + 1] = { type = "unlock-recipe", recipe = "empty-recipe" }
+  iron_processing.effects[#iron_processing.effects + 1] = { type = "unlock-recipe", recipe = "empty-shotgun-recipe" }
 end
 
 if mods["lignumis"] and mods["aai-industry"] then
