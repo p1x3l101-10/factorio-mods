ada842daea3c473b754b5e79abf018f5f8fbeabb
Pixel <scott.blatt.0b10@gmail.com>
2025-03-29 17:39:24 -0500

    cleanup

--- tweaks/compatibility/multi/prototypes/recipies.lua
+++ tweaks/compatibility/multi/prototypes/recipies.lua
@@ -1,6 +1,15 @@
-if mods["wood-military"] and mods["More_Ammo"] then
-  local wood_cost = settings.startup["wood-military-ammo-cost"].value
-  if settings.startup["wood-military-smg-ammo"].value then
-    data.raw["recipe"]["wood-darts-magazine"].ingredients = {{type="item", name="wood", amount=wood_cost}}
+local Recipe = require("__cf-lib__/data/Recipe")
+
+if mods["wood-military"] and mods["More_Ammo"] and settings.startup["wood-military-smg-ammo"].value then
+  Recipe:new("wood-darts-magazine"):removeIngredient("empty-magazine")
+  Recipe:new("wood-shotgun-shell"):removeIngredient("empty-shotgun-shell")
+end
+
+if mods["lignumis"] and mods["More_Ammo"] then
+  Recipe:new("empty-recipe"):unlockedByTechnology("iron-processing")
+  Recipe:new("empty-shotgun-recipe"):unlockedByTechnology("iron-processing")
+  if settings.startup["high-capacity-magazines"] then
+    Recipe:new("empty-high-capacity-magazine"):unlockedByTechnology("iron-processing")
+    Recipe:new("empty-high-capacity-shotgun-shell"):unlockedByTechnology("iron-processing")
   end
 end
--- tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
+++ tweaks/compatibility/multi/prototypes/technology-final-fixes.lua
@@ -1,21 +1,9 @@
-if mods["alloy-smelting"] and mods["lignumis"] and not mods["aai-industry"] then
-  data.raw["technology"]["kiln-smelting"].prerequisites = { "automation-science-pack" }
-end
+local Technology = require("__cf-lib__/data/Technology")
 
-if mods["lignumis"] and mods["More_Ammo"] then
-  local recipes = data.raw["recipe"]
-  recipes["empty-recipe"].enabled = false
-  recipes["empty-shotgun-recipe"].enabled = false
-  local iron_processing = data.raw["technology"]["iron-processing"].effects
-  local copper_processing = data.raw["technology"]["copper-processing"].effects
-  iron_processing[#iron_processing + 1] = { type = "unlock-recipe", recipe = "empty-recipe" }
-  copper_processing[#copper_processing + 1] = { type = "unlock-recipe", recipe = "empty-shotgun-recipe" }
+if mods["alloy-smelting"] and mods["lignumis"] and not mods["aai-industry"] then
+  Technology:new("burner-mechanics"):addPrerequisite("automation-science-pack")
 end
 
 if mods["lignumis"] and mods["aai-industry"] then
-  local red_science = data.raw["technology"]["automation-science-pack"].prerequisites
-  if not red_science
-    red_science = {}
-  end
-  red_science[#red_science + 1] = { "copper-processing" }
+  Technology:new("automation-science-pack"):addPrerequisite("copper-processing")
 end
