960cbcd09c006a0a2e9f5ca067adf78698dc766d
Pixel <scott.blatt.0b10@gmail.com>
2025-03-31 11:34:19 -0500

    correct old qualities too

--- mythic-mark/prototypes/technology-updates.lua
+++ mythic-mark/prototypes/technology-updates.lua
@@ -1,1 +1,4 @@
 data.raw.technology["mythic-quality"].icon = "__mythic-mark-quality__/graphics/technology/mythic-quality.png"
+data.raw.technology["mythic-quality"].localised_name = "Mark 6 machines"
+data.raw.technology["legendary-quality"].localised_name = "Mark 5 machines"
+data.raw.technology["epic-quality"].localised_name = "Mark 4 machines"
