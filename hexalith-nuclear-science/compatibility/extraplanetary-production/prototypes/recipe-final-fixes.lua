4c4e69ce40492077fc68c5008c1f7e42df220fb0
Pixel <scott.blatt.0b10@gmail.com>
2025-04-08 12:30:53 -0500

    cripe

--- hexalith-nuclear-science/compatibility/extraplanetary-production/prototypes/recipe-final-fixes.lua
+++ hexalith-nuclear-science/compatibility/extraplanetary-production/prototypes/recipe-final-fixes.lua
@@ -1,4 +1,4 @@
-local util = require("functions").tech
+local util = require("functions").recipe
 if settings.startup["basic-science-changes"].value then
   util.set_category("advanced-military-science-pack", "centrifuging")
   -- Remove other recipes
