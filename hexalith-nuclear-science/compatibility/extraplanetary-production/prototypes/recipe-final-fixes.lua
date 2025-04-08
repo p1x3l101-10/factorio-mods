d9b94f085d12b1cf3ad791291f80b5e86a5464ef
Pixel <scott.blatt.0b10@gmail.com>
2025-04-08 12:27:40 -0500

    add more compat

---
+++ hexalith-nuclear-science/compatibility/extraplanetary-production/prototypes/recipe-final-fixes.lua
@@ -0,0 +1,10 @@
+local util = require("functions").te
+if settings.startup["basic-science-changes"].value then
+  util.set_category("advanced-military-science-pack", "centrifuging")
+  -- Remove other recipes
+  if settings.startup["basic-science-changes"].value then
+    util.set_category("automation-science-pack", "assembling-machine")
+    util.set_category("logistic-science-pack", "assembling-machine")
+    util.set_category("military-science-pack", "assembling-machine")
+  end
+end
--- hexalith-nuclear-science/data-final-fixes.lua
+++ hexalith-nuclear-science/data-final-fixes.lua
@@ -1,1 +1,5 @@
 require("prototypes.worldgen-final-fixes")
+
+if mods["extraplanetary-production"] then
+  require("compatibility.extraplanetary-production.data-final-fixes")
+end
