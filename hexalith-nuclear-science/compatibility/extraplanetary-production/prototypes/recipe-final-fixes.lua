6aac24105afe8bed1ff4b4b86bfd9312114d6823
Pixel <scott.blatt.0b10@gmail.com>
2025-04-08 12:32:53 -0500

    cats

--- hexalith-nuclear-science/compatibility/extraplanetary-production/prototypes/recipe-final-fixes.lua
+++ hexalith-nuclear-science/compatibility/extraplanetary-production/prototypes/recipe-final-fixes.lua
@@ -3,8 +3,8 @@ if settings.startup["basic-science-changes"].value then
   util.set_category("advanced-military-science-pack", "centrifuging")
   -- Remove other recipes
   if settings.startup["basic-science-changes"].value then
-    util.set_category("automation-science-pack", "assembling-machine")
-    util.set_category("logistic-science-pack", "assembling-machine")
-    util.set_category("military-science-pack", "assembling-machine")
+    util.set_category("automation-science-pack", "crafting")
+    util.set_category("logistic-science-pack", "crafting")
+    util.set_category("military-science-pack", "crafting")
   end
 end
