fb738126adc92fd99ce5235a0fb163fc291104ca
Pixel <scott.blatt.0b10@gmail.com>
2025-04-08 09:16:49 -0500

    make the burner crusher upgrade

--- tweaks/compatibility/multi/prototypes/entity-final-fixes.lua
+++ tweaks/compatibility/multi/prototypes/entity-final-fixes.lua
@@ -1,3 +1,7 @@
 if mods["lignumis"] and mods["aai-loaders"] and mods["aai-loaders-electric-powered"] then
   energy_source = nil
 end
+
+if mods["crushing-industry"] then
+  data.raw["furnace"]["burner-crusher"].next_upgrade = "electric-crusher"
+end
