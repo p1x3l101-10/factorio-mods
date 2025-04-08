e4be5e91b100c1f5a0cc9025206c9c2fb3430874
Pixel <scott.blatt.0b10@gmail.com>
2025-04-08 14:28:09 -0500

    signify the upstreaming of a feature

--- tweaks/compatibility/multi/prototypes/entity.lua
+++ tweaks/compatibility/multi/prototypes/entity.lua
@@ -1,4 +1,4 @@
-if mods["crushing-industry"] then
+if mods["crushing-industry"] > "0.3.9" then -- Merged upstream
   data.raw["furnace"]["electric-crusher"].fast_replaceable_group = "electric-crusher"
   data.raw["furnace"]["burner-crusher"].fast_replaceable_group = "electric-crusher"
   data.raw["furnace"]["burner-crusher"].next_upgrade = "electric-crusher"
