69cb4d79f94881a3390ea635cdee2589b5506ebb
Pixel <scott.blatt.0b10@gmail.com>
2025-04-10 00:24:58 -0500

    bugfixes

--- hexalith-nuclear-science/compatibility/SchallUraniumProcessing/prototypes/technology-updates.lua
+++ hexalith-nuclear-science/compatibility/SchallUraniumProcessing/prototypes/technology-updates.lua
@@ -1,4 +1,4 @@
 local util = require("functions").tech
 
-util.set_prerequisites("uranium-processing", "planet-discovery-hexalith")
+util.set_prerequisites("uranium-processing", { "planet-discovery-hexalith" })
 util.disable_tech("uranium-mining")
