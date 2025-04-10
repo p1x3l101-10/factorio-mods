8cd4c6ec8398083ebee1850ea6b360ca198b5c04
Pixel <scott.blatt.0b10@gmail.com>
2025-04-10 00:27:13 -0500

    bugfixes

---
+++ hexalith-nuclear-science/prototypes/surfaces-updates.lua
@@ -0,0 +1,4 @@
+data.raw['optimized-decorative']['nerve-roots-veins-dense'] = table.deepcopy(data.raw['optimized-decorative']['cream-nerve-roots-veins-dense'])
+data.raw['optimized-decorative']['nerve-roots-veins-dense'].name = 'nerve-roots-veins-dense'
+data.raw['optimized-decorative']['nerve-roots-veins-sparse'] = table.deepcopy(data.raw['optimized-decorative']['cream-nerve-roots-veins-sparse'])
+data.raw['optimized-decorative']['nerve-roots-veins-sparse'].name = 'nerve-roots-veins-sparse'
