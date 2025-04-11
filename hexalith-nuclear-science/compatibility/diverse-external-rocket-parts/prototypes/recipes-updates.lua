956cee55a0674540393bb6a1743e8e5e38ad2cb4
Pixel <scott.blatt.0b10@gmail.com>
2025-04-11 14:28:03 -0500

    fix array

--- hexalith-nuclear-science/compatibility/diverse-external-rocket-parts/prototypes/recipes-updates.lua
+++ hexalith-nuclear-science/compatibility/diverse-external-rocket-parts/prototypes/recipes-updates.lua
@@ -8,38 +8,40 @@ end
 
 -- Create hexalith rocket recipe
 data:extend({
-  type = "recipe",
-  name = "rocket-part-hexalith",
-  localised_name = {"item-name.rocket-part"},
-  localised_description = {"item-description.rocket-part"},
-  energy_required = 5,
-  enabled = false,
-  hide_from_player_crafting = false,
-  category = "centrifuging",
-  subgroup = "space-rocket",
-  ingredients = {
-    {type = "item", name = "processing-unit", amount = 2*settings.startup["external-rocket-part-cost-setting"].value },
-    {type = "item", name = "low-density-structure", amount = 2*settings.startup["external-rocket-part-cost-setting"].value },
-    {type = "item", name = "nuclear-fuel", amount = settings.startup["external-rocket-part-cost-setting"].value },
-    {type = "item", name = uraniumRocketItem, amount = 10*settings.startup["external-rocket-part-cost-setting"].value }
-  },
-  results = {{type="item", name="rocket-part", amount=1}},
-  allow_productivity = true,
-  allow_quality = false,
-  icons = {
-    {icon = "__base__/graphics/icons/rocket-part.png", icon_size=64},
-    {icon = "__hexalith-nuclear-science__/graphics/technology/hexalith.png", icon_size=64, scale=0.25, shift={-8,8}}
-  },
-  surface_conditions = {
-    {
-      property = "pressure",
-      min = 1000,
-      max = 1000
+  {
+    type = "recipe",
+    name = "rocket-part-hexalith",
+    localised_name = {"item-name.rocket-part"},
+    localised_description = {"item-description.rocket-part"},
+    energy_required = 5,
+    enabled = false,
+    hide_from_player_crafting = false,
+    category = "centrifuging",
+    subgroup = "space-rocket",
+    ingredients = {
+      {type = "item", name = "processing-unit", amount = 2*settings.startup["external-rocket-part-cost-setting"].value },
+      {type = "item", name = "low-density-structure", amount = 2*settings.startup["external-rocket-part-cost-setting"].value },
+      {type = "item", name = "nuclear-fuel", amount = settings.startup["external-rocket-part-cost-setting"].value },
+      {type = "item", name = uraniumRocketItem, amount = 10*settings.startup["external-rocket-part-cost-setting"].value }
     },
-    { -- Ensure hexalith
-      property = "gravity",
-      min = 12,
-      max = 12
+    results = {{type="item", name="rocket-part", amount=1}},
+    allow_productivity = true,
+    allow_quality = false,
+    icons = {
+      {icon = "__base__/graphics/icons/rocket-part.png", icon_size=64},
+      {icon = "__hexalith-nuclear-science__/graphics/technology/hexalith.png", icon_size=64, scale=0.25, shift={-8,8}}
+    },
+    surface_conditions = {
+      {
+        property = "pressure",
+        min = 1000,
+        max = 1000
+      },
+      { -- Ensure hexalith
+        property = "gravity",
+        min = 12,
+        max = 12
+      }
     }
   }
 })
