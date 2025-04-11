107be660f6f1bd3f1f4f309fbd31222b89eb5c43
Pixel <scott.blatt.0b10@gmail.com>
2025-04-11 17:35:31 -0500

    register stuff for derp

--- hexalith-nuclear-science/control.lua
+++ hexalith-nuclear-science/control.lua
@@ -3,3 +3,6 @@ local handler = require("event_handler")
 handler.add_libraries({
   require("scripts.difficulty")
 })
+
+-- Add planet for DERP
+remote.call("diverse_external_rocket_parts", "add_custom_surface", "hexalith")
