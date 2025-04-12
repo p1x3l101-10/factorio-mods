1cbcdcccea5eee0eb538a00e1897d1dd3bbedd8a
Pixel <scott.blatt.0b10@gmail.com>
2025-04-12 14:15:51 -0500

    fix endings

--- tweaks/compatibility/multi/prototypes/recipes-final-fixes.lua
+++ tweaks/compatibility/multi/prototypes/recipes-final-fixes.lua
@@ -1,14 +1,14 @@
 if mods["maraxsis"] and mods["diverse-external-rocket-parts"] then
   -- Disable normal maraxis recipe
-    local function removeEffect(techName, effect)
-      local old_effects = data.raw["technology"][techName].effects
-      local deleted = false
-      local new_effects = {}
-      for _, effect in pairs(old_effects) do
-        if effect[1] == effect then
-          deleted = true
-        else
-          new_effects[#new_effects + 1] = effect
+  local function removeEffect(techName, effect)
+    local old_effects = data.raw["technology"][techName].effects
+    local deleted = false
+    local new_effects = {}
+    for _, effect in pairs(old_effects) do
+      if effect[1] == effect then
+        deleted = true
+      else
+        new_effects[#new_effects + 1] = effect
       end
       if deleted then
         old_effects = new_effects
@@ -24,29 +24,29 @@ if mods["maraxsis"] and mods["diverse-external-rocket-parts"] then
   -- New rocket part
   local rocketItem = require("__p1x3l101-shared-parts__/lib").optional.diverse_external_rocket_parts
   local waterRocket = rocketItem:new("maraxsis")
-    waterRocket:setIngredients(
-      {
-        { "processing-unit", 2 },
-        { "low-density-structure", 1 },
-        { "rocket-fuel", 1 },
-        { "maraxsis-super-sealant-substance", 1 }
-      },
-      {
-        { "processing-unit", 10 },
-        { "low-density-structure", 3 },
-        { "rocket-fuel", 3 },
-        { "maraxsis-glass-panes", 5 },
-        { "maraxsis-salt", },
-        { "maraxsis-super-sealant-substance", 5 }
-      }
-    )
-    waterRocket:addPlanetImage("__planet-maraxis__/graphics/technology/maraxis.png", 256)
-    waterRocket:unlockedBy("maraxsis-project-seadragon")
-    waterRocket:setCrafter("maraxsis-hydro-plant-or-assembling")
-    waterRocket:surfaceConditions(
-      {
-        { property = "pressure", min = 200000, max = 200000 }
-      }
-    )
-    waterRocket:apply()
-else
+  waterRocket:setIngredients(
+    {
+      { "processing-unit", 2 },
+      { "low-density-structure", 1 },
+      { "rocket-fuel", 1 },
+      { "maraxsis-super-sealant-substance", 1 }
+    },
+    {
+      { "processing-unit", 10 },
+      { "low-density-structure", 3 },
+      { "rocket-fuel", 3 },
+      { "maraxsis-glass-panes", 5 },
+      { "maraxsis-salt", },
+      { "maraxsis-super-sealant-substance", 5 }
+    }
+  )
+  waterRocket:addPlanetImage("__planet-maraxis__/graphics/technology/maraxis.png", 256)
+  waterRocket:unlockedBy("maraxsis-project-seadragon")
+  waterRocket:setCrafter("maraxsis-hydro-plant-or-assembling")
+  waterRocket:surfaceConditions(
+    {
+      { property = "pressure", min = 200000, max = 200000 }
+    }
+  )
+  waterRocket:apply()
+end
