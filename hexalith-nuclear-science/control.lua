a3e710f6c180e2e2288352051d60af7cba0f7b95
Pixel <scott.blatt.0b10@gmail.com>
2025-04-11 21:36:16 -0500

    remove file

--- hexalith-nuclear-science/control.lua
+++ hexalith-nuclear-science/control.lua
@@ -3,6 +3,3 @@ local handler = require("event_handler")
 handler.add_libraries({
   require("scripts.difficulty")
 })
-
--- Add planet for DERP
-remote.call("diverse_external_rocket_parts", "add_custom_surface", "hexalith")
