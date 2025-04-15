5ef4305e9d7aec092c75b02d39bd9e51fc236f74
Pixel <scott.blatt.0b10@gmail.com>
2025-04-15 15:07:11 -0500

    add debug stuff

--- shared-parts/control.lua
+++ shared-parts/control.lua
@@ -1,16 +1,13 @@
 local bigunpack = require("__big-data-string2__.unpack")
-local function discover_applied_surfaces()
-  local decode = serpent.load
-  local function getData(name)
-    return decode(bigunpack(name))
-  end
-  local planets = getData("DERP_Planet_Rockets")
-  return planets
+local decode = serpent.load
+local function getData(name)
+  return decode(bigunpack(name))
 end
 
 script.on_configuration_changed(function()
   if not script.active_mods["diverse-external-rocket-parts"] then return end
-  for _, surface in pairs(discover_applied_surfaces()) do
+  for _, surface in ipairs(getData("DERP_Planet_Rockets")) do
+    log("Registered DERP surface: ".. surface)
     remote.call("diverse_external_rocket_parts", "add_custom_surface", surface)
   end
 end)
